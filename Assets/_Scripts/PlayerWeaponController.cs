d0d9387ce535f2257f37a9b96070bf5baa25233c
DESKTOP-L8PKNRH\ADMIN <nguyevanlong22072003@gmail.com>
2025-11-09 12:34:50 +0700

    Weapon Reload Animation 2(fix)

--- Assets/_Scripts/PlayerWeaponController.cs
+++ Assets/_Scripts/PlayerWeaponController.cs
@@ -5,6 +5,8 @@ public class PlayerWeaponController : MonoBehaviour
     private Animator animator;
     private PlayerInput playerInput;
 
+    const string STR_FIRE_TRIGGER = "Fire";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +23,6 @@ public class PlayerWeaponController : MonoBehaviour
     }
     private void Shoot()
     {
-        animator.SetTrigger("Fire");
+        animator.SetTrigger(STR_FIRE_TRIGGER);
     }
 }
